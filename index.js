05e5b0765429c3009d1b0289454603484e04008d
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-04-12 22:46:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,12 @@ function johnLennonFacts(facts) {
 }    
 
 
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
 
+     return love;
+}
