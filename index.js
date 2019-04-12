92baf2bcd5f375fb60ed7695a5c40ff4fa99a843
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-04-12 21:25:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
+
+function johnLennonFacts(facts) {
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
+}    
+  
+function iLoveTheBeatles() {
+  var i = 1;
+  do {                        
+     i *= 2;                     
+    document.write(i + ", ");   
+} while (i < 100)
+}
